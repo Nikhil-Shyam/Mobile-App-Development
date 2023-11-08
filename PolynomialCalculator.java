39d7dc926d7176624068381c70f7e2c984fd0407
Nikhil-Shyam <115193450+Nikhil-Shyam@users.noreply.github.com>
2023-11-08 13:31:12 -0500

    Update PolynomialCalculator.java

--- PolynomialCalculator.java
+++ PolynomialCalculator.java
@@ -4,64 +4,72 @@ import java.util.Collections;
 
 public class PolynomialCalculator{
 	String input;
+	boolean error = false;
 	ArrayList<String> addSubTerms = new ArrayList<String>();
 	ArrayList<String> multiplicationOutput = new ArrayList<String>();
 	ArrayList<String> signs = new ArrayList<String>();
 	int signCounter = 0;
 	String addSubInput = "";
-	boolean once = true;
 	ArrayList<String> addAndSub = new ArrayList<String>();
 	ArrayList<Integer> descendingVariableOrder = new ArrayList<Integer>();
 	ArrayList<String> outputArray = new ArrayList<String>();
+	String output = "";
 
 	PolynomialCalculator(){
-		input = "2x^3-4x^2-12x^3+x-8x^2+5x-3-5*3";
-		// input = "-x^2+32x-3*4x-12x^2-1";
+		// input = "2x^3-4x^2-12x^3+x-8x^2+5x-3-5*3";
+		// input = "x-x^2+32x-3*4x-12x^2-1";
 		// input = "3x+2x^2";
-		// input = "8+8x+8x^2";
-
+		// input = "8x-8x+8x^2-2x*4x+3-3";
+		// input = "9*3x^2+6x^7-3x^2*6x^4+12x^8-6x^2+3*8-6+5";
+		input = "8x^2-3x^2+4x^2-8*3x^2";
 
 		System.out.println(input + "\n");
 
+		checkForErrors();
 
-		// breaking inp into terms (add/sub) and adding to arraylist
-		StringTokenizer st1 = new StringTokenizer(input, "+-");
-		while (st1.hasMoreTokens()){
-			addSubTerms.add(st1.nextToken());
+		if (error){
+			System.out.println("Error");
+		}else{
+			// breaking inp into terms (add/sub) and adding to arraylist
+			StringTokenizer st1 = new StringTokenizer(input, "+-");
+			while (st1.hasMoreTokens()){
+				addSubTerms.add(st1.nextToken());
+			}
+
+			multiply();
+
+			// determining signs
+			determineSigns();
+
+			// putting back into a string for addition/subtraction
+			addSubString();
+
+			// breaking up addition and subtraction
+			addSubTerms = new ArrayList<String>();
+			StringTokenizer st2 = new StringTokenizer(addSubInput, "+-");
+			signCounter = 0;
+
+			while(st2.hasMoreTokens()){
+				addSubTerms.add(st2.nextToken());
+			}
+
+			addSubTermSigns();
+
+			// addition and subtraction
+			additionAndSubtraction();
+
+			descendingOrder();
+
+			for(int i = 0; i < outputArray.size(); i++)
+				output += outputArray.get(i);
+
+			if (output.length() == 0)
+				output = "0";
+			else if (output.charAt(0) == '+')
+				output = output.substring(1);
+
+			System.out.println(output);
 		}
-
-		multiply();
-
-		// determining signs
-		determineSigns();
-
-		// putting back into a string for addition/subtraction
-		addSubString();
-
-		// breaking up addition and subtraction
-		addSubTerms = new ArrayList<String>();
-		StringTokenizer st2 = new StringTokenizer(addSubInput, "+-");
-		signCounter = 0;
-
-		while(st2.hasMoreTokens()){
-			addSubTerms.add(st2.nextToken());
-		}
-
-		addSubTermSigns();
-
-		// addition and subtraction
-		additionAndSubtraction();
-
-		descendingOrder();
-
-		String output = "";
-		for(int i = 0; i < outputArray.size(); i++)
-			output += outputArray.get(i);
-
-		if (output.charAt(0) == '+')
-			output = output.substring(1);
-
-		System.out.println(output);
 	}
 
 	public static void main(String[] args){
@@ -167,6 +175,12 @@ public class PolynomialCalculator{
 			else
 				variable = 0;
 
+			int cons = 0;
+			if (variable >= 1)
+				cons = Integer.parseInt(temp.substring(0, temp.indexOf("x")));
+			else
+				cons = Integer.parseInt(temp);
+
 			int tempSize = addSubTerms.size();
 			for (int j = addSubTerms.size()-1; j > i; j--){
 				int variable2 = 0;
@@ -179,50 +193,46 @@ public class PolynomialCalculator{
 					variable2 = 0;
 
 				if (variable == variable2){
-					int cons = 0;
 					int cons2 = 0;
-					if (variable >= 1){
-						cons = Integer.parseInt(temp.substring(0, temp.indexOf("x")));
+					if (variable >= 1)
 						cons2 = Integer.parseInt(temp2.substring(0, temp2.indexOf("x")));
-					}else{
-						cons = Integer.parseInt(temp);
+					else
 						cons2 = Integer.parseInt(temp2);
-					}
 
-					if (variable > 1){
-						if (cons+cons2 > 0)
-							addAndSub.add((cons+cons2)+"x^"+variable);
-						else if (cons + cons2 < 0)
-							addAndSub.add((cons+cons2)+"x^"+variable);
-					}
-					else if (variable == 1){
-						if (cons+cons2 > 0)
-							addAndSub.add("+" + (cons+cons2)+"x");
-						else if (cons + cons2 < 0)
-							addAndSub.add((cons+cons2)+"x");
-					}
-					else if (variable == 0){
-						if (cons+cons2 > 0)
-							addAndSub.add("+" + (cons+cons2));
-						else if (cons + cons2 < 0)
-							addAndSub.add(Integer.toString(cons+cons2));
-					}
+					cons += cons2;
+
 					addSubTerms.remove(j);
 				}
 			}
+
 			if (tempSize == addSubTerms.size()){
 				if (temp.charAt(0) == '-')
 					addAndSub.add(addSubTerms.get(i));
 				else
 					addAndSub.add("+" + addSubTerms.get(i));
+			}else if (variable > 1){
+				if (cons > 0)
+					addAndSub.add("+" + (cons)+"x^"+variable);
+				else if (cons < 0)
+					addAndSub.add((cons)+"x^"+variable);
+			}else if (variable == 1){
+				if (cons > 0)
+					addAndSub.add("+" + (cons)+"x");
+				else if (cons < 0)
+					addAndSub.add((cons)+"x");
+			}else if (variable == 0){
+				if (cons > 0)
+					addAndSub.add("+" + (cons));
+				else if (cons < 0)
+					addAndSub.add(Integer.toString(cons));
 			}
 		}
 
 		for (int i = 0; i < addAndSub.size(); i++){
 			String temp = addAndSub.get(i);
 			if (temp.contains("x")){
-				if (temp.substring(0, temp.indexOf("x")).equals("1"))
-					addAndSub.set(i, temp.substring(temp.indexOf("x")));
+				if (temp.substring(0, temp.indexOf("x")).equals("+1"))
+					addAndSub.set(i, "+" + temp.substring(temp.indexOf("x")));
 				if (temp.substring(0, temp.indexOf("x")).equals("-1"))
 					addAndSub.set(i, "-" + temp.substring(temp.indexOf("x")));
 			}
@@ -243,7 +253,6 @@ public class PolynomialCalculator{
 		}
 		Collections.sort(descendingVariableOrder, Collections.reverseOrder());
 
-
 		for(int i = 0; i < descendingVariableOrder.size(); i++){
 			int tempD = descendingVariableOrder.get(i);
 			for (int j = 0; j < addAndSub.size(); j++){
@@ -257,6 +266,38 @@ public class PolynomialCalculator{
 					tempF = 0;
 				if (tempF == tempD)
 					outputArray.add(temp);
+			}
+		}
+	}
+
+	public void checkForErrors(){
+		char last = input.charAt(input.length()-1);
+		if(last == '+' || last == '*' || last == '^' || last == '-')
+			error = true;
+
+		for (int i = 0; i < input.length()-1; i++){
+			char temp = input.charAt(i);
+			if (temp == '+' || temp == '^' || temp == 'x' || temp == '-' || temp == '*'){
+				if (temp == input.charAt(i+1))
+					error = true;
+			}
+			if (temp == '^'){
+				char temp2 = input.charAt(i+1);
+				if (temp2 == 'x' || temp2 == '+' || temp2 == '-' || temp2 == '*')
+					error = true;
+			}
+			if (temp == '-'){
+				if (input.charAt(i+1) == '+')
+					error = true;
+			}
+			if (temp == '+'){
+				if (input.charAt(i+1) == '-')
+					error = true;
+			}
+			if (i == 0){
+				char first = input.charAt(i);
+				if(first == '+' || first == '*' || first == '^')
+					error = true;
 			}
 		}
 	}
