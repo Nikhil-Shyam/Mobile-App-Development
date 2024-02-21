375a318edd427758c8c0d8ce1fe9608dff9b4586
Nikhil-Shyam <115193450+Nikhil-Shyam@users.noreply.github.com>
2024-02-21 09:10:36 -0500

    Update OpenWeather.java

--- OpenWeather.java
+++ OpenWeather.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.EditText;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -17,49 +19,66 @@ import java.net.URL;
 import java.net.URLConnection;
 
 public class MainActivity extends AppCompatActivity {
-    URL url;
-    URLConnection urlConnection;
-    InputStream inputStream;
-    BufferedReader bufferedReader;
-    String geoInfo, fiveDayInfo = "";
+    URL url1, url2;
+    URLConnection urlConnection1, urlConnection2;
+    InputStream inputStream1, inputStream2;
+    BufferedReader bufferedReader1, bufferedReader2;
+    String geoInfo = "";
+    String fiveDayInfo = "";
+    EditText zipcode;
     String temp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        zipcode = findViewById(R.id.editTextZipcode);
+        String inp = zipcode.toString();
         AsyncThread task = new AsyncThread();
         task.execute();
     }
-    public class AsyncThread extends AsyncTask<Void, Void, Void> {
+    public class AsyncThread extends AsyncTask<String, Void, Void> {
         @Override
-        protected Void doInBackground(Void... voids) {
+        protected Void doInBackground(String... voids) {
             try{
-                url = new URL("https://api.openweathermap.org/geo/1.0/zip?zip=08852,US&appid=cfcdedfa1e43c6123cff4f3374adb61a");
-                urlConnection = url.openConnection();
-                inputStream = urlConnection.getInputStream();
-                bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                // url = new URL("https://api.openweathermap.org/geo/1.0/zip?zip=" + voids + ",US&appid=cfcdedfa1e43c6123cff4f3374adb61a");
+                url1 = new URL("https://api.openweathermap.org/geo/1.0/zip?zip=08824,US&appid=cfcdedfa1e43c6123cff4f3374adb61a");
+                urlConnection1 = url1.openConnection();
+                inputStream1 = urlConnection1.getInputStream();
+                bufferedReader1 = new BufferedReader(new InputStreamReader(inputStream1));
 
-                String st;
-                while ((st = bufferedReader.readLine()) != null)
-                    geoInfo += st;
+                Log.d("url", url1 + "");
 
-                url = new URL("https://api.openweathermap.org/data/2.5/forecast?zip=08852,US&appid=cfcdedfa1e43c6123cff4f3374adb61a");
-                urlConnection = url.openConnection();
-                inputStream = urlConnection.getInputStream();
-                bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-
-                st = "";
-                while ((st = bufferedReader.readLine()) != null)
-                    fiveDayInfo += st;
+                String st1;
+                while ((st1 = bufferedReader1.readLine()) != null)
+                    geoInfo += st1;
 
                 JSONObject geoWeather = new JSONObject(geoInfo);
-                JSONObject fiveDayWeather = new JSONObject(fiveDayInfo);
-                temp = geoWeather.get("zip").toString();
+                String lat = geoWeather.get("lat").toString();
+                String lon = geoWeather.get("lon").toString();
+
+                Log.d("LAT", lat);
+                Log.d("LON", lon);
+
+                url2 = new URL("api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=cfcdedfa1e43c6123cff4f3374adb61a");
+                urlConnection2 = url2.openConnection();
+                inputStream2 = urlConnection2.getInputStream();
+                bufferedReader2 = new BufferedReader(new InputStreamReader(inputStream2));
+
+                Log.d("url", url2 + "");
+
+                String st2;
+                while ((st2 = bufferedReader2.readLine()) != null) {
+                    Log.d("st2", st2);
+                    fiveDayInfo += st2;
+                }
+
+
+                // JSONArray fiveDayWeather = new JSONArray(fiveDayInfo);
             }catch(IOException | JSONException e){}
+
             // This is where you will download your data.
             // You will need to override another method to update the UI
-            Log.d("API", fiveDayInfo);
-            // Log.d("ZIP", temp);
+            Log.d("5day", fiveDayInfo);
 
             return null;
         }
